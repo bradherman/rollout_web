61775dbf61985fcf84c32ae16a0568a69c0e7c05
Bradley Herman <bradley.t.herman@gmail.com>
2014-05-19 12:00:10 -0700

    Hash tweak for backwards compat

--- app/controllers/rollout_web/application_controller.rb
+++ app/controllers/rollout_web/application_controller.rb
@@ -10,7 +10,9 @@ module RolloutWeb
 
     def authorize
       if ENV["RESQUE_WEB_HTTP_BASIC_AUTH_USER"] && ENV["RESQUE_WEB_HTTP_BASIC_AUTH_PASSWORD"]
-        authenticate_or_request_with_http_basic {|u, p| u == ENV["RESQUE_WEB_HTTP_BASIC_AUTH_USER"] && p == ENV["RESQUE_WEB_HTTP_BASIC_AUTH_PASSWORD"] }
+        authenticate_or_request_with_http_basic  do |u, p| 
+          u == ENV["RESQUE_WEB_HTTP_BASIC_AUTH_USER"] && p == ENV["RESQUE_WEB_HTTP_BASIC_AUTH_PASSWORD"]
+        end
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ RolloutWeb::Engine.routes.draw do
   RolloutWeb::Plugins.plugins.each do |p|
     mount p::Engine => p.engine_path
   end
-  post 'flag/:id', to: 'flags#update'
-  root to: 'flags#index'
+  post 'flag/:id', :to => 'flags#update'
+  root :to => 'flags#index'
 end
